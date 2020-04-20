5cdd55face56c38df5be941269b7d1973a5e4f19
Sergi Pérez <sergi.perez@iesjoandaustria.org>
2020-04-20 08:50:15 +0200

    S'afegeix arxiu base on poden començar a programar

---
+++ boira.py
@@ -0,0 +1,47 @@
+#Exercici2
+def comptarParaules(text):
+  #TODO Heu de retornar el nombre de paraules que té un text
+  #Com podeu detectar una paraula, que el separa les paraules?
+
+def comptarFrases(text):
+  #TODO Heu de retornar el nombre de frases. 
+  #Com es separen les frases?
+
+def mitjanaParaulesPerFrase(text):
+  #TODO Heu de retornar la mitjana de paraules per frases?
+
+#Exercici 3
+def numeroParulesComplexes(text):
+  #TODO Heu de retornar el nombre de paraules complexes que té el text
+  #Són aquelles que tenen méés de cinc lletres
+
+def percetantgeParaulesComplexes(text):
+
+
+#TODO Fer-ho al final de tot el 5.
+#Exericic 5 llegir configuració
+
+#Exercici 1
+nomFitxer = input("Entra el nom de l'arxiu que vols analitzar")
+print("Gràcies!")
+
+#TODO heu de completar el codi perquè la variable text tingui el contingut del fitxer
+
+text = 
+
+#Per provar exercici 2 i 3 podeu posar-vos print aquíí per comprovar si es compta béé paraules, si compta frases, si es fa la mitja i si es detecten les complexes.
+
+#Exercici 4
+#càlcul de la fórmula
+index = 
+
+#TODO  Pinteu index per pantalla 
+
+
+#TODO Classifiqueu exercici segons índex
+# Digueu com és el text seguint la taula https://en.wikipedia.org/wiki/Gunning_fog_inde
+
+#TODO
+#Heu de tancar el fitxer
+
+
